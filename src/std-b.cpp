861dadb9ea6849873ab8451a1f57036a693c1e7e
reedacartwright <cartwright@asu.edu>
2014-02-19 18:30:05 -0700

    added std algorithms

---
+++ src/std-b.cpp
@@ -0,0 +1,57 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+#include "xorshift64.h"
+#include "rexp.h"
+#include "rbeta.h"
+
+// implement reservoir as a vector
+typedef std::vector<int> reservoir;
+
+using namespace std;
+
+int main( int argc, const char* argv[] ) {
+	if(argc < 4) {
+		cerr << "Usage: " << argv[0] << 
+			" stream-size sample-size number-of-samples" << endl;
+		return 1;
+	}
+	int stream_size = static_cast<int>(strtod(argv[1],NULL));
+	int sample_size = static_cast<int>(strtod(argv[2],NULL));
+	int sample_num = static_cast<int>(strtod(argv[3],NULL));
+	
+	// construct the main RNG
+	xorshift64 rng(3294533022);
+		
+	for(int g=0;g<sample_num;++g) {
+		// construct the stream RNG
+		xorshift64 stream(2693652924);
+
+		reservoir res;
+		res.reserve(sample_size);
+		// setup a table to hold cummulative weights
+		std::vector<double> table;
+
+		// Algorithm STD-B
+		// First Pass
+		double totalw = 0.0;
+		for(int i=0;i<stream_size;++i) {
+			totalw += stream.get_double52();
+			table.push_back(totalw);
+		}
+		
+		// Second Pass, reset stream
+		stream.seed(2693652924);
+		
+		for(int i=0;i<sample_size;++i) {
+			double w = totalw*rng.get_double52();
+			auto it = lower_bound(table.begin(),table.end(),w);
+			res.push_back(it-table.begin());
+		}
+		cout << res.back() << endl;
+	}
+	return 0;
+}
