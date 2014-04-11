23eca3cb9feb9d2fbdd8c194a9284316e04fe577
reedacartwright <cartwright@asu.edu>
2014-04-11 11:05:29 -0700

    removed comment

--- src/rnormal.h
+++ src/rnormal.h
@@ -19,46 +19,6 @@
 extern const double ytab[256];
 extern const double ktab[256];
 extern const double wtab[256];
- 
-/* position of right-most step */
-// #define PARAM_R 3.44428647676
-
-/*
-double rand_normal_org(xorshift64 &rng, double mu, double sigma) {
-	uint32_t i, j;
-	int sign;
-	double x, y;
-
-	for(;;) {
-		uint32_t k = rng.get_uint32();
-		i = (k & 0xFF);
-		j = (k >> 8) & 0xFFFFFF;
-
-		sign = (i & 0x80) ? +1 : -1;
-		i &= 0x7f;
-
-		x = j * wtab[i];
-
-		if (j < ktab[i])
-			break;
-
-		if(i < 127) {
-			double y0 = ytab[i], y1 = ytab[i + 1];
-			double U1 = rng.get_double52();
-			y = y1 + (y0 - y1) * U1;
-		} else {
-			double U1 = rng.get_double52();
-			double U2 = rng.get_double52();
-			x = PARAM_R - log(U1) / PARAM_R;
-			y = exp(-PARAM_R * (x - 0.5 * PARAM_R)) * U2;
-		}
-
-		if (y < exp(-0.5 * x * x))
-			break;
-    }
-	return mu + sign * sigma * x;	
-}
-*/
 
 inline double rand_normal(xorshift64 &rng, double mu=0.0, double sigma=1.0) {
 	const double R = 3.6554204190269415;
